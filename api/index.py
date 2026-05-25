2375f415548ddeeff10e6109e5850735432d0c48
sauravbhojak <sauravbhojak6156@gmail.com>
2026-05-25 23:38:39 +0530

    Fix TemplateResponse

--- api/index.py
+++ api/index.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
@@ -18,7 +17,7 @@ app.mount(
     name="static"
 )
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse(
         "index.html",

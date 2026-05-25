218a7f9c0e7314cc5732935b25989ccca4949d88
sauravbhojak <sauravbhojak6156@gmail.com>
2026-05-25 23:46:14 +0530

    Fix Vercel TemplateResponse signature

--- api/index.py
+++ api/index.py
@@ -20,6 +20,7 @@ app.mount(
 @app.get("/")
 async def home(request: Request):
     return templates.TemplateResponse(
-        "index.html",
-        {"request": request}
+        request=request,
+        name="index.html",
+        context={}
     )

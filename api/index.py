01becb0f92ae1fec6241ae4947e23b5c6e2f1516
sauravbhojak <sauravbhojak6156@gmail.com>
2026-05-25 23:21:08 +0530

    Fix TemplateResponse argument order

--- api/index.py
+++ api/index.py
@@ -2,12 +2,21 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 app = FastAPI()
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(
+    directory=os.path.join(BASE_DIR, "templates")
+)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount(
+    "/static",
+    StaticFiles(directory=os.path.join(BASE_DIR, "static")),
+    name="static"
+)
 
 
 @app.get("/", response_class=HTMLResponse)
